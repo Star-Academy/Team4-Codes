d628b213d8ceb3247a1db1a9fc4080ec0daf10b1
Danyal Farahany <45311261+Danyal0096@users.noreply.github.com>
2020-08-04 10:07:20 +0430

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,16 @@ import java.util.*;
 public class Main {
 
 
-    public static void main(String[] args) {
-        //driver code
-        
-        InvertedIndex allTokens = new InvertedIndex();
-        allTokens.fillMap(); //put all words and their respective docs in the reversed index map
-        
-        //input
+    static String[] inputFromUser(){
         System.out.println("Enter word to search");
         Scanner sc = new Scanner(System.in);
         String keyLine = sc.nextLine();
+        sc.close();
         keyLine = keyLine.toLowerCase();
-        String[] keywords = keyLine.split(" ");
+        return keyLine.split(" ");
+    }
 
-        //calculate final output based on user's input (signs handled)
-        OutputCalculator co = new OutputCalculator(allTokens);
-        Set<String> output = co.returnOutput(keywords);
-        
-        //output
+    static void output(Set<String> output){
         if (output.size() != 0) {
             for (String a : output) {
                 System.out.print(a + " ");
@@ -28,6 +20,20 @@ public class Main {
         } else {
             System.out.println("no result found");
         }
+    }
 
+    public static void main(String[] args) {
+        //driver code
+        
+        InvertedIndex allTokens = new InvertedIndex();
+        allTokens.fillMap(); //put all words and their respective docs in the reversed index map
+        
+        String[] keywords = inputFromUser();
+
+        //calculate final output based on user's input (signs handled)
+        OutputCalculator co = new OutputCalculator(allTokens);
+        Set<String> output = co.returnOutput(keywords);
+        
+        output(output);
     }
 }
