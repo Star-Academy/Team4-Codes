1de831d8a85a46db34ea00a7108070c271933f61
Saba <saba.eb18@gmail.com>
2020-08-11 12:25:18 +0430

    linq parts

--- StartingCS/phase4-T4/library/DataBaseReader.cs
+++ StartingCS/phase4-T4/library/DataBaseReader.cs
@@ -9,21 +9,21 @@ namespace library
 {
     public class DataBaseReader
     {
-        private string StudentsPath{get; set;}
-        private string ScoresPath{get; set;}
-        public DataBaseReader(string studentsPath, string scoresPath){
+        private string StudentsPath { get; set; }
+        private string ScoresPath { get; set; }
+        public DataBaseReader(string studentsPath, string scoresPath)
+        {
             StudentsPath = studentsPath;
             ScoresPath = scoresPath;
         }
-        public List<Student> ReadStudentsData(){
+        public List<Student> ReadStudentsData()
+        {
             var scoresJson = File.ReadAllText(ScoresPath);
             var studentsJson = File.ReadAllText(StudentsPath);
             var students = JsonSerializer.Deserialize<List<Student>>(studentsJson);
             var scores = JsonSerializer.Deserialize<List<StudentScore>>(scoresJson);
             Dictionary<int, int> studentsIndices = new Dictionary<int, int>();
-            for (int i = 0; i < students.Count; i++){
-                studentsIndices.Add(students[i].StudentNumber, i);
-            }
+            studentsIndices = students.Select((n, i) => new { n, i }).ToDictionary(x => x.n.StudentNumber, x => x.i);
             foreach (StudentScore score in scores){
                 students[studentsIndices[score.StudentNumber]].UpdateScores(score);
             }
