e9160a91c5995faebb2aa906c0b616e3251fe833
Saba <saba.eb18@gmail.com>
2020-08-11 10:19:28 +0430

    symbols correction

--- StartingCS/phase4-T4/driver-program/Program.cs
+++ StartingCS/phase4-T4/driver-program/Program.cs
@@ -11,11 +11,11 @@ namespace driver_program
     {
         private const string Scores_Path = "Scores.json";
         private const string Students_Path = "Students.json";
-        private const int Best_To_Take_Count = 3;
+        private const int BestToTakeCount = 3;
         static void Main(string[] args)
         {
             var students = (new DataBaseReader(Students_Path, Scores_Path)).ReadStudentsData();
-            foreach (Student student in students.OrderByDescending(s => s.Average).Take(Best_To_Take_Count)){
+            foreach (Student student in students.OrderByDescending(s => s.Average).Take(BestToTakeCount)){
                 Console.WriteLine("{0} {1} {2}", student.FirstName, student.LastName, student.Average);
             }
         }
