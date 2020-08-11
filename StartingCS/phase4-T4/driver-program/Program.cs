8a44177f606781f59682b3ea730f5674087e07cc
Danyal Farahany <farahany.danyal@gmail.com>
2020-08-11 09:21:23 +0430

    little const

--- StartingCS/phase4-T4/driver-program/Program.cs
+++ StartingCS/phase4-T4/driver-program/Program.cs
@@ -11,10 +11,11 @@ namespace driver_program
     {
         private const string Scores_Path = "Scores.json";
         private const string Students_Path = "Students.json";
+        private const int Best_To_Take_Count = 3;
         static void Main(string[] args)
         {
             var students = (new DataBaseReader(Students_Path, Scores_Path)).ReadStudentsData();
-            foreach (Student student in students.OrderByDescending(s => s.Average).Take(3)){
+            foreach (Student student in students.OrderByDescending(s => s.Average).Take(Best_To_Take_Count)){
                 Console.WriteLine("{0} {1} {2}", student.FirstName, student.LastName, student.Average);
             }
         }
