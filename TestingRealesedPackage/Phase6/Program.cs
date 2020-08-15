ce8d55959ceec3e66a0ee18c019e2977242a9489
Danyal Farahany <farahany.danyal@gmail.com>
2020-08-15 16:25:01 +0430

    phase6Complete

---
+++ TestingRealesedPackage/Phase6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SearchLibrary;
+namespace Phase6
+{
+    class Program
+    {
+        private const string EnglishDataDirectory = "..\\..\\SearchEngineCS\\Phase5\\EnglishData";
+        static void Main(string[] args)
+        {
+            var app = new SearchApp(EnglishDataDirectory);
+            app.Start();
+        }
+    }
+}
