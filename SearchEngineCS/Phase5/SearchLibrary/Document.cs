ec8b94680b6cec66f8d1a1fc53d8687772f920ea
Danyal Farahany <farahany.danyal@gmail.com>
2020-08-15 13:25:28 +0430

    sth

--- SearchEngineCS/Phase5/SearchLibrary/Document.cs
+++ SearchEngineCS/Phase5/SearchLibrary/Document.cs
@@ -8,12 +8,14 @@ namespace SearchLibrary
 
         public override bool Equals(object obj)
         {
-            if (obj == null || GetType() != obj.GetType())
-                return false;
-            Document doc = (Document)obj;
-            return this.Id.Equals(doc.Id) && this.Content.Equals(doc.Content);
+            return obj is Document document &&
+                   Id == document.Id &&
+                   Content == document.Content;
         }
 
-    
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Content);
+        }
     }
 }
